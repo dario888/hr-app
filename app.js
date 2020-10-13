af253fa425d012defc653536def4a57ce28cc8cd
dario888 <dario.bario87@hotmail.com>
2020-10-13 10:17:33 +0200

    Create server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const connectDB = require('./connectDB');
+
+
+
+
+
+const app = express();
+
+app.listen(4000, () => console.log('APP IS RUNNING ON PORT 4000'));
