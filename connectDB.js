ccc91799c6f5b792885a878ddeaa488bd0924048
dario888 <dario.bario87@hotmail.com>
2020-10-13 10:00:09 +0200

    Create react & DB

---
+++ connectDB.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+const DB = process.env.DATABASE
+
+
+const connectDB = () => {
+    mongoose.connect(DB, {
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useFindAndModify: false, 
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB CONNECTED'))
+    .catch(err => {
+        console.error('ERROR CONNECTING', err.message);
+        process.exit(1);//exitingthe app  with failor
+    });
+};
+
+
+module.exports = connectDB;
